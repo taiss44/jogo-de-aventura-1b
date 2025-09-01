f086f20f11706df07b000de69172fb38e25b6939
taiss44 <tais.matuchaki.moreira@escola.pr.gov.br>
2025-09-01 17:01:52 -0300

    add açôes no botão

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = document.querySelectorA11('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function () {
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getATTribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+
+    })
+})
